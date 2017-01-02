a316353f1787d2b01b31590726b15865321895ae
H3rrero <jandromix@hotmail.com>
2017-01-02 01:03:44 +0100

    Ultimas correcciones de angular charts

--- prueba.js
+++ prueba.js
@@ -2,9 +2,29 @@
   'use strict'
 angular.module('Prueba',["chart.js"])
 .controller('PruebaController',PruebaController)
-.controller("DoughnutCtrl", function ($scope) {
-  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.data = [300, 500, 100];
+.controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["1000", "2000", "3000", "5000", "7000", "9000", "10000"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [100, 250, 310, 600, 745, 864, 943]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        }
+      ]
+    }
+  };
 })
 .directive('myMap', function() {
     // directive link function
