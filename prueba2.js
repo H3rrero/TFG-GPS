e83da227f454887de9de6d64f0bd5f3f96258e61
H3rrero <jandromix@hotmail.com>
2017-01-02 21:15:31 +0100

    Ajuste de tamaño del contenedor datos
    se han ajustado los tamaños y añadido funcion scroll para intentar que
    se los datos se vean bien al reducirse el tamaño del navegador.

--- prueba2.js
+++ prueba2.js
@@ -1,4 +1,24 @@
-angular.module("app", ["chart.js"]).controller("DoughnutCtrl", function ($scope) {
-  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.data = [300, 500, 100];
+angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["1000", "2000", "3000", "5000", "7000", "9000", "10000"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [100, 250, 310, 600, 745, 864, 943]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        }
+      ]
+    }
+  };
 });
