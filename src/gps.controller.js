1df72be5154d8083c7f4910304cac84eda8cd3e2
H3rrero <jandromix@hotmail.com>
2017-02-19 14:21:47 +0100

    prueba

--- src/gps.controller.js
+++ src/gps.controller.js
@@ -847,7 +847,7 @@ list1.dowXmlForR = function () {
     list1.dataUrl = 'data:xml/plain;charset=utf-8,'
       + encodeURIComponent(xml);
 
-      window.open('data:xml/plain;charset=utf-8;Content-Disposition:attachment;filename=fname.gpx,' +encodeURIComponent(xml));
+      //window.open('data:application/octet-stream,' +encodeURIComponent(xml));
     }
     else{
       console.log(list1.fichero);
